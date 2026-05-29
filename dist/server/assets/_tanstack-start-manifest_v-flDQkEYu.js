e10bff988f6811d9008754e7ac4feb096d4cc367
Theo <tpanayiotou01@gmail.com>
2026-05-29 11:10:57 +0300

    Fix gitignore and remove build artifacts

--- dist/server/assets/_tanstack-start-manifest_v-flDQkEYu.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/theo/Desktop/TheMrDev/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-nbytWMm7.js"] }, "/": { filePath: "/Users/theo/Desktop/TheMrDev/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-7kGnjsxF.js"] } }, clientEntry: "/assets/index-nbytWMm7.js" });
-export {
-  tsrStartManifest
-};
